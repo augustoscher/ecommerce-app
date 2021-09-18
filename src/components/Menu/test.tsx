2c0d8e1122b9023d02e31232f673f7ca87bdfa2f
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-09-17 22:27:32 -0300

    test: add tests for Menu component

--- src/components/Menu/test.tsx
+++ src/components/Menu/test.tsx
@@ -1,17 +1,15 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
 import Menu from './index'
 
 describe('<Menu />', () => {
-  it('match snapshot', () => {
-    const { container } = render(<Menu />)
+  it('should render the menu', () => {
+    renderWithTheme(<Menu />)
 
-    expect(container).toMatchSnapshot()
-  })
-
-  it('should render the heading', () => {
-    render(<Menu />)
-
-    expect(screen.getByRole('heading', { name: /Menu/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(screen.getByRole('img', { name: /won games/i })).toBeInTheDocument()
+    expect(screen.getByLabelText(/search/i)).toBeInTheDocument()
+    expect(screen.getByLabelText(/open shopping cart/i)).toBeInTheDocument()
   })
 })
