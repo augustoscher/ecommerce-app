927cff1fd8f1fd041d274867d7db52e9059f6623
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-09-20 09:57:09 -0300

    feat: remove useless props

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -6,11 +6,9 @@ type ButtonTypes =
   | ButtonHTMLAttributes<HTMLButtonElement>
 
 export type ButtonProps = {
-  children?: React.ReactNode
   size?: 'small' | 'medium' | 'large'
   fullWidth?: boolean
   icon?: JSX.Element
-  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void
   as?: React.ElementType
 } & ButtonTypes
 
