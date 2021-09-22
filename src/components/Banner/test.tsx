52c81cdc20419f48b73c3895f4db0df30b3efb42
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-09-21 23:02:40 -0300

    feat: adding Banner component

---
+++ src/components/Banner/test.tsx
@@ -0,0 +1,25 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import Banner from '.'
+
+describe('<Banner />', () => {
+  it('should render correctly', () => {
+    renderWithTheme(
+      <Banner
+        img="https://source.unsplash.com/user/willianjusten/1042x580"
+        title="Defy death"
+        subtitle="<p>Play the new <strong>CrashLands</strong> season"
+        buttonLabel="Buy now"
+        buttonLink="/games/defy-death"
+      />
+    )
+
+    // verifique se o title existe renderizado (.toBeInTheDocument())
+    expect(screen.getByText(/Defy death/i)).toBeInTheDocument()
+    // verifique se o subtitle existe renderizado
+    // expect(screen.getByText(/Defy death/i)).toBeInTheDocument()
+    // verifique se a imagem existe renderizado
+    expect(screen.getByRole('link', { name: /buy now/i })).toBeInTheDocument()
+  })
+})
