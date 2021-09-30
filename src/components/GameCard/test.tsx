226444ad477f3277398bc2546af2914812b71aad
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-09-29 21:12:09 -0300

    test: add tests to GameCard component

--- src/components/GameCard/test.tsx
+++ src/components/GameCard/test.tsx
@@ -1,19 +1,32 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
-import GameCard from './index'
+import GameCard from '.'
+
+const props = {
+  title: 'Population Zero',
+  developer: 'Rockstar Games',
+  img: 'https://source.unsplash.com/user/willianjusten/300x140',
+  price: 'R$ 235,00'
+}
 
 describe('<GameCard />', () => {
-  it('match snapshot', () => {
-    const { container } = render(<GameCard />)
-
-    expect(container).toMatchSnapshot()
-  })
-
-  it('should render the heading', () => {
-    render(<GameCard />)
+  it('should render correctly', () => {
+    renderWithTheme(<GameCard {...props} />)
 
     expect(
-      screen.getByRole('heading', { name: /GameCard/i })
+      screen.getByRole('heading', { name: props.title })
     ).toBeInTheDocument()
+
+    expect(
+      screen.getByRole('heading', { name: props.developer })
+    ).toBeInTheDocument()
+
+    expect(screen.getByRole('img', { name: props.title })).toHaveAttribute(
+      'src',
+      props.img
+    )
+
+    expect(screen.getByLabelText(/add to wishlist/i)).toBeInTheDocument()
   })
 })
