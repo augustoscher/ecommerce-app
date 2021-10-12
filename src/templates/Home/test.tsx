a2f0afe736c67bae267179841a0bca943ce1fd1a
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-10-11 21:34:33 -0300

    test: add tests for Home template

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -1,19 +1,46 @@
-import { render, screen } from '@testing-library/react'
+import 'match-media-mock'
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
 
-import Home from './index'
+import Home, { HomeTemplateProps } from './index'
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+const data: HomeTemplateProps = {
+  banners: bannersMock,
+  newGames: gamesMock,
+  mostPopularHighlight: highlightMock,
+  mostPopularGames: gamesMock,
+  upcommingGames: gamesMock,
+  upcommingHighligth: highlightMock,
+  upcommingMoreGames: gamesMock,
+  freeGames: gamesMock,
+  freeHighligth: highlightMock
+}
 
 describe('<Home />', () => {
-  it('match snapshot', () => {
-    const { container } = render(<Home />)
+  // it('should render menu and footer', () => {
+  //   renderWithTheme(<Home {...data} />)
 
-    expect(container).toMatchSnapshot()
-  })
+  //   expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+  //   expect(
+  //     screen.getByRole('heading', { name: /contact us/i })
+  //   ).toBeInTheDocument()
+  // })
 
-  it('should render the heading', () => {
-    render(<Home />)
+  it('should render the sections', () => {
+    renderWithTheme(<Home {...data} />)
 
+    expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
     expect(
-      screen.getByRole('heading', { name: /Home/i })
+      screen.getByRole('heading', { name: /most popular/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /upcomming/i })
+    ).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /free games/i })
     ).toBeInTheDocument()
   })
 })
