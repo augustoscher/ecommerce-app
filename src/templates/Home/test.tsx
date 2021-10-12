db9ceb92742aa01274400153863bfb620d70b97f
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-10-12 01:20:50 -0300

    test: add tests for Home template

--- src/templates/Home/test.tsx
+++ src/templates/Home/test.tsx
@@ -20,14 +20,15 @@ const data: HomeTemplateProps = {
 }
 
 describe('<Home />', () => {
-  // it('should render menu and footer', () => {
-  //   renderWithTheme(<Home {...data} />)
+  it('should render menu and footer', () => {
+    renderWithTheme(<Home {...data} />)
 
-  //   expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
-  //   expect(
-  //     screen.getByRole('heading', { name: /contact us/i })
-  //   ).toBeInTheDocument()
-  // })
+    expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
+    expect(
+      screen.getByRole('heading', { name: /follow us/i })
+    ).toBeInTheDocument()
+    expect(screen.getAllByRole('img', { name: /won games/i })).toHaveLength(2)
+  })
 
   it('should render the sections', () => {
     renderWithTheme(<Home {...data} />)
@@ -42,5 +43,15 @@ describe('<Home />', () => {
     expect(
       screen.getByRole('heading', { name: /free games/i })
     ).toBeInTheDocument()
+  })
+
+  it('should render section elements', () => {
+    renderWithTheme(<Home {...data} />)
+    // banner
+    expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
+    // card game ( 5 sections com 4 cards cada = 5x4 = 20)
+    expect(screen.getAllByText(/population zero/i)).toHaveLength(20)
+    // highlight
+    expect(screen.getAllByText(/read dead is back!/i)).toHaveLength(3)
   })
 })
