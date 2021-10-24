d0212badc22c1872ef112f90d483aad8017865c7
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-10-24 01:49:59 -0300

    feat: add onCheck prop - controlled component

--- src/components/Checkbox/stories.tsx
+++ src/components/Checkbox/stories.tsx
@@ -4,6 +4,9 @@ import Checkbox, { CheckboxProps } from '.'
 export default {
   title: 'Checkbox',
   component: Checkbox,
+  argTypes: {
+    onCheck: { action: 'checked' }
+  },
   parameters: {
     backgrounds: {
       default: 'won-dark'
