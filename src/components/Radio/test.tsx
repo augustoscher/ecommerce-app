fcb23e71bcd846ee49261f72731ad820377ebc8a
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-10-26 17:00:41 -0300

    test: add Radio component tests

---
+++ src/components/Radio/test.tsx
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react'
+
+import Radio from './index'
+
+describe('<Radio />', () => {
+  it('match snapshot', () => {
+    const { container } = render(<Radio />)
+
+    expect(container).toMatchSnapshot()
+  })
+
+  it('should render the heading', () => {
+    render(<Radio />)
+
+    expect(
+      screen.getByRole('heading', { name: /Radio/i })
+    ).toBeInTheDocument()
+  })
+})
