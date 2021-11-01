b5c8209657ca184648bdb674702adbf3a99eb1b8
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-11-01 11:32:41 -0300

    feat: add Radio component

--- src/components/Radio/test.tsx
+++ src/components/Radio/test.tsx
@@ -5,13 +5,11 @@ import Radio from './index'
 describe('<Radio />', () => {
   it('match snapshot', () => {
     const { container } = render(<Radio />)
-
     expect(container).toMatchSnapshot()
   })
 
   it('should render the heading', () => {
     render(<Radio />)
-
     expect(screen.getByRole('heading', { name: /Radio/i })).toBeInTheDocument()
   })
 })
