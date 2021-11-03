a035856ac9629503fe083299cb201ec38d39b0b2
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-11-03 08:43:10 -0300

    feat: add Radio component

--- src/components/Radio/index.tsx
+++ src/components/Radio/index.tsx
@@ -1,15 +1,45 @@
 import { InputHTMLAttributes } from 'react'
+
 import * as S from './styles'
 
-export type RadioProps = {
-  name?: string
-  value?: string
-} & InputHTMLAttributes<HTMLInputElement> //let pass by props other properties of input
+type RadioValue = string | ReadonlyArray<string> | number
 
-const Radio = ({ name, value = '', ...props }: RadioProps) => (
-  <S.Wrapper>
-    <S.Input type="radio" name={name} value={value} {...props} />
-  </S.Wrapper>
-)
+export type RadioProps = {
+  onCheck?: (value?: RadioValue) => void
+  label?: string
+  labelColor?: 'white' | 'black'
+  labelFor?: string
+  value?: RadioValue
+} & InputHTMLAttributes<HTMLInputElement>
+
+const Radio = ({
+  label,
+  onCheck,
+  labelColor = 'white',
+  labelFor = '',
+  value,
+  ...props
+}: RadioProps) => {
+  const onChange = () => {
+    !!onCheck && onCheck(value)
+  }
+
+  return (
+    <S.Wrapper>
+      <S.Input
+        id={labelFor}
+        type="radio"
+        value={value}
+        onChange={onChange}
+        {...props}
+      />
+      {!!label && (
+        <S.Label labelColor={labelColor} htmlFor={labelFor}>
+          {label}
+        </S.Label>
+      )}
+    </S.Wrapper>
+  )
+}
 
 export default Radio
