8a7e3732172a3c5ebd220ad9a6743cf26ff9a544
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-11-09 09:57:44 -0300

    feat: add TextField component stories

--- src/components/TextField/stories.tsx
+++ src/components/TextField/stories.tsx
@@ -1,9 +1,23 @@
 import { Story, Meta } from '@storybook/react/types-6-0'
-import TextField from '.'
+import TextField, { TextFieldProps } from '.'
 
 export default {
   title: 'TextField',
   component: TextField,
+  args: {
+    label: 'E-mail',
+    labelFor: 'Email',
+    id: 'Email',
+    initialValue: '',
+    placeholder: 'john.cage@gmail.com'
+  },
+  argTypes: {
+    onInput: { action: 'changed' }
+  }
 } as Meta
 
-export const Usage: Story = () => <TextField />
+export const Default: Story<TextFieldProps> = (args) => (
+  <div style={{ maxWidth: 350, padding: 15 }}>
+    <TextField {...args} />
+  </div>
+)
