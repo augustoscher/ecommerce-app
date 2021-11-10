5bedfd14b5e7c7e862afbcbb1e2feb98b345903c
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-11-10 15:18:15 -0300

    feat: add TextField component

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -6,9 +6,13 @@ export type TextFieldProps = {
   label?: string
   labelFor?: string
   initialValue?: string
+  icon?: React.ReactNode
+  iconPosition?: 'left' | 'right'
 } & InputHTMLAttributes<HTMLInputElement>
 
 const TextField = ({
+  icon,
+  iconPosition = 'left',
   onInput,
   label,
   labelFor = '',
@@ -27,12 +31,14 @@ const TextField = ({
     <S.Wrapper>
       {!!label && <S.Label htmlFor={labelFor}>{label}</S.Label>}
       <S.InputWrapper>
+        {!!icon && <S.Icon iconPosition={iconPosition}>{icon}</S.Icon>}
         <S.Input
           type="text"
           onChange={onChange}
           value={value}
+          iconPosition={iconPosition}
           {...props}
-        ></S.Input>
+        />
       </S.InputWrapper>
     </S.Wrapper>
   )
--- src/components/TextField/styles.ts
+++ src/components/TextField/styles.ts
@@ -1,5 +1,9 @@
 import styled, { css } from 'styled-components'
 
+import { TextFieldProps } from '.'
+
+type IconPositionProps = Pick<TextFieldProps, 'iconPosition'>
+
 export const InputWrapper = styled.div`
   ${({ theme }) => css`
     display: flex;
@@ -16,12 +20,13 @@ export const InputWrapper = styled.div`
   `}
 `
 
-export const Input = styled.input`
-  ${({ theme }) => css`
+export const Input = styled.input<IconPositionProps>`
+  ${({ theme, iconPosition }) => css`
     color: ${theme.colors.black};
     font-family: ${theme.font.family};
     font-size: ${theme.font.sizes.medium};
     padding: ${theme.spacings.xxsmall} 0;
+    padding-${iconPosition}: ${theme.spacings.xsmall};
     background: transparent;
     border: 0;
     outline: none;
@@ -34,6 +39,18 @@ export const Label = styled.label`
     font-size: ${theme.font.sizes.small};
     color: ${theme.colors.black};
     cursor: pointer;
+  `}
+`
+
+export const Icon = styled.div<IconPositionProps>`
+  ${({ theme, iconPosition }) => css`
+    display: flex;
+    width: 2.2rem;
+    color: ${theme.colors.gray};
+    order: ${iconPosition === 'right' ? 1 : 0};
+    & > svg {
+      width: 100%;
+    }
   `}
 `
 
