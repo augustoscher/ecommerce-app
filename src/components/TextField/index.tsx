5aede7f8a6bdd7ae5d50611849301c301a55ca9e
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-11-10 21:41:12 -0300

    feat: add TextField component

--- src/components/TextField/index.tsx
+++ src/components/TextField/index.tsx
@@ -8,11 +8,13 @@ export type TextFieldProps = {
   initialValue?: string
   icon?: React.ReactNode
   iconPosition?: 'left' | 'right'
+  disabled?: boolean
 } & InputHTMLAttributes<HTMLInputElement>
 
 const TextField = ({
   icon,
   iconPosition = 'left',
+  disabled = false,
   onInput,
   label,
   labelFor = '',
@@ -28,7 +30,7 @@ const TextField = ({
   }
 
   return (
-    <S.Wrapper>
+    <S.Wrapper disabled={disabled}>
       {!!label && <S.Label htmlFor={labelFor}>{label}</S.Label>}
       <S.InputWrapper>
         {!!icon && <S.Icon iconPosition={iconPosition}>{icon}</S.Icon>}
@@ -37,6 +39,7 @@ const TextField = ({
           onChange={onChange}
           value={value}
           iconPosition={iconPosition}
+          disabled={disabled}
           {...props}
         />
       </S.InputWrapper>
