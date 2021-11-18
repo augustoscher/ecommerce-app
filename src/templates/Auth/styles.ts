24d2d6ef1fb3e5bb34694bd6f28a7e5a72fe4ff7
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-11-18 00:16:35 -0300

    feat: add Auth template

--- src/templates/Auth/styles.ts
+++ src/templates/Auth/styles.ts
@@ -1,3 +1,11 @@
 import styled from 'styled-components'
 
 export const Wrapper = styled.main``
+
+export const BannerBlock = styled.div``
+
+export const Subtitle = styled.h3``
+
+export const Footer = styled.footer``
+
+export const Content = styled.div``
