e1bb822eea7901aa43cef311db0d5c1031cb3e86
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-12-01 22:54:15 -0300

    feat: add FormSignIn component

---
+++ src/components/FormSignIn/stories.tsx
@@ -0,0 +1,9 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import FormSignIn from '.'
+
+export default {
+  title: 'FormSignIn',
+  component: FormSignIn,
+} as Meta
+
+export const Usage: Story = () => <FormSignIn />
