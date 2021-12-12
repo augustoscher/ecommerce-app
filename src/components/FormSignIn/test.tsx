6eb561f40248bc29518a070e805ee9655a7ebcb7
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-12-12 12:19:49 -0300

    test: form sign in component

--- src/components/FormSignIn/test.tsx
+++ src/components/FormSignIn/test.tsx
@@ -4,18 +4,27 @@ import FormSignIn from '.'
 
 describe('<FormSignIn />', () => {
   it('should render the form', () => {
-    renderWithTheme(<FormSignIn />)
+    const { container } = renderWithTheme(<FormSignIn />)
+
     expect(screen.getByPlaceholderText('Email')).toBeInTheDocument()
     expect(screen.getByPlaceholderText('Password')).toBeInTheDocument()
     expect(
       screen.getByRole('button', { name: /Sign in now/i })
     ).toBeInTheDocument()
+
+    expect(container.parentElement).toMatchSnapshot()
   })
 
-  // it('should render the forgot password link', () => {})
+  it('should render the forgot password link', () => {
+    renderWithTheme(<FormSignIn />)
+    expect(
+      screen.getByRole('link', { name: /forgot your password\?/i })
+    ).toBeInTheDocument()
+  })
 
-  it('should render the text and link to sign up', () => {
-    // text
-    // link
+  it('should render text to sign up if already have an account', () => {
+    renderWithTheme(<FormSignIn />)
+    expect(screen.getByRole('link', { name: /sign up/i })).toBeInTheDocument()
+    expect(screen.getByText(/don’t have an account\?/i)).toBeInTheDocument()
   })
 })
