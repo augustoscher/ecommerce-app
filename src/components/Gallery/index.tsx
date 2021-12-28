8b738163864e17e4427dd627b75482e4c60bf5e4
Augusto S. Scher <augusto@jusbrasil.com.br>
2021-12-28 13:52:56 -0300

    feat: add open/close events on Gallery component

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,5 +1,8 @@
+import { useEffect, useState } from 'react'
+
 import { ArrowBackIos as ArrowLeft } from '@styled-icons/material-outlined/ArrowBackIos'
 import { ArrowForwardIos as ArrowRight } from '@styled-icons/material-outlined/ArrowForwardIos'
+import { Close } from '@styled-icons/material-outlined/Close'
 
 import Slider, { SliderSettings } from 'components/Slider'
 
@@ -49,19 +52,45 @@ export type GalleryProps = {
   items: GalleryImageProps[]
 }
 
-const Gallery = ({ items }: GalleryProps) => (
-  <S.Wrapper>
-    <Slider settings={settings}>
-      {items.map((item, index) => (
-        <img
+const Gallery = ({ items }: GalleryProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  // add event listener when hook is mounted (component mount)
+  useEffect(() => {
+    const handleKeyUp = ({ key }: KeyboardEvent) => {
+      key === 'Escape' && setIsOpen(false)
+    }
+    window.addEventListener('keyup', handleKeyUp)
+
+    //cleanup - remove listener
+    return () => window.removeEventListener('keyup', handleKeyUp)
+  }, [])
+
+  return (
+    <S.Wrapper>
+      <Slider settings={settings}>
+        {items.map((item, index) => (
+          <img
+            role="button"
+            key={`thumb-${index}`}
+            src={item.src}
+            alt={`Thumb - ${item.label}`}
+            onClick={() => setIsOpen(true)}
+          />
+        ))}
+      </Slider>
+
+      <S.Modal isOpen={isOpen} aria-label="modal" aria-hidden={!isOpen}>
+        <S.Close
           role="button"
-          key={`thumb-${index}`}
-          src={item.src}
-          alt={`Thumb - ${item.label}`}
-        />
-      ))}
-    </Slider>
-  </S.Wrapper>
-)
+          aria-label="close modal"
+          onClick={() => setIsOpen(false)}
+        >
+          <Close size={40} />
+        </S.Close>
+      </S.Modal>
+    </S.Wrapper>
+  )
+}
 
 export default Gallery
