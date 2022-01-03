cc94926a5f08b601df432aebf98a61b2dfde5cdc
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-01-03 16:10:16 -0300

    feat: add Games template component

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -1,9 +1,45 @@
+import Base from 'templates/Base'
+
+import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined/KeyboardArrowDown'
+
+import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar'
+import GameCard, { GameCardProps } from 'components/GameCard'
+import { Grid } from 'components/Grid'
+
 import * as S from './styles'
 
-const Games = () => (
-  <S.Wrapper>
-    <h1>Games</h1>
-  </S.Wrapper>
-)
+export type GamesTemplateProps = {
+  games?: GameCardProps[]
+  filterItems: ItemProps[]
+}
 
-export default Games
+const GamesTemplate = ({ filterItems, games = [] }: GamesTemplateProps) => {
+  const handleFilter = () => {
+    return
+  }
+
+  const handleShowMore = () => {
+    return
+  }
+
+  return (
+    <Base>
+      <S.Main>
+        <ExploreSidebar items={filterItems} onFilter={handleFilter} />
+        <section>
+          <Grid>
+            {games.map((item) => (
+              <GameCard key={item.title} {...item} />
+            ))}
+          </Grid>
+
+          <S.ShowMore role="button" onClick={handleShowMore}>
+            <p>Show More</p>
+            <ArrowDown size={35} />
+          </S.ShowMore>
+        </section>
+      </S.Main>
+    </Base>
+  )
+}
+export default GamesTemplate
