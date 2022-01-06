9be9014226e81ac69e2df1bde2ff39c1fd2609f9
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-01-06 09:17:13 -0300

    test: add Dropdownd component tests

--- src/components/Dropdown/test.tsx
+++ src/components/Dropdown/test.tsx
@@ -1,17 +1,33 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
+import { renderWithTheme } from 'utils/tests/helpers'
 
-import Dropdown from './index'
+import Dropdown from '.'
 
 describe('<Dropdown />', () => {
-  it('should render the heading', () => {
-    render(
-      <Dropdown title="click me">
-        <h1>asd</h1>
+  beforeEach(() => {
+    const title = <h1 aria-label="toogle dropdown">Click here</h1>
+
+    renderWithTheme(
+      <Dropdown title={title}>
+        <span>content</span>
       </Dropdown>
     )
+  })
 
-    expect(
-      screen.getByRole('heading', { name: /Dropdown/i })
-    ).toBeInTheDocument()
+  it('should render title', () => {
+    expect(screen.getByLabelText(/toogle dropdown/)).toBeInTheDocument()
+  })
+
+  it('should handle open/close dropdown', () => {
+    const content = screen.getByText(/content/).parentElement!
+
+    expect(content).toHaveStyle({ opacity: 0 })
+    expect(content.getAttribute('aria-hidden')).toBe('true')
+
+    userEvent.click(screen.getByLabelText(/toogle dropdown/))
+
+    expect(content).toHaveStyle({ opacity: 1 })
+    expect(content.getAttribute('aria-hidden')).toBe('false')
   })
 })
