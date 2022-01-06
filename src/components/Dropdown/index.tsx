b716f92864ab02c61128e6f174b287a1f8294827
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-01-06 09:17:46 -0300

    feat: add Dropdown component

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import * as S from './styles'
 
 export type DropdownProps = {
@@ -6,10 +7,12 @@ export type DropdownProps = {
 }
 
 const Dropdown = ({ title, children }: DropdownProps) => {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
-    <S.Wrapper>
-      <S.Title>{title}</S.Title>
-      <S.Content>{children}</S.Content>
+    <S.Wrapper isOpen={isOpen}>
+      <S.Title onClick={() => setIsOpen(!isOpen)}>{title}</S.Title>
+      <S.Content aria-hidden={!isOpen}>{children}</S.Content>
     </S.Wrapper>
   )
 }
