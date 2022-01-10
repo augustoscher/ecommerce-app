2eceef6d25c334d79e72b5174281316cdc2d87b1
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-01-10 10:00:24 -0300

    feat: use Apollo via SSR

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,39 +1,40 @@
-import { gql, useQuery } from '@apollo/client'
+import { gql } from '@apollo/client'
 
 import Home, { HomeTemplateProps } from 'templates/Home'
 import bannersMock from 'components/BannerSlider/mock'
 import gamesMock from 'components/GameCardSlider/mock'
 import highlightMock from 'components/Highlight/mock'
 
-export default function Index(props: HomeTemplateProps) {
-  const { data, loading, error } = useQuery(gql`
-    query getGames {
-      games {
-        name
-      }
-    }
-  `)
+import { initializeApollo } from 'utils/apollo'
 
-  console.log(data)
-  console.log(loading)
-  console.log(error)
+const GET_GAMES = gql`
+  query getGames {
+    games {
+      name
+    }
+  }
+`
+export default function Index(props: HomeTemplateProps) {
+  if (props.data) return <p>{JSON.stringify(props.data, null, 2)}</p>
+
   return <Home {...props} />
 }
-
 // ATENÇÃO:
 // os métodos getStaticProps/getServerSideProps SÓ FUNCIONAM EM PAGES
 
-// getStaticProps => gerar estático em build time
-// getServerSideProps => gerar via ssr a cada request
-// getInitialProps => gerar via ssr a cada request
+// getStaticProps => gerar estático em build time (mesmo que o gatsby)
+// getServerSideProps => gerar via ssr a cada request (nunca vai para o bundle do client)
+// getInitialProps => gerar via ssr a cada request (vai para o client, faz hydrate do lado do client depois do 1 req)
 export async function getServerSideProps() {
-  // faz lógica
-  // pode ser buscar dados numa API
-  // fazer calculo|leitura de context
+  const apolloClient = initializeApollo()
+
+  const { data } = await apolloClient.query({ query: GET_GAMES })
 
   // retorno dos dados
   return {
     props: {
+      data: data,
+      initialApolloState: apolloClient.cache.extract(),
       banners: bannersMock,
       newGames: gamesMock,
       mostPopularHighlight: highlightMock,
