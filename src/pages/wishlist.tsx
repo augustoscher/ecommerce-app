19d955405bd65571fb77d2a728e37d56dd35d4eb
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-01-19 23:34:52 -0300

    feat: fix wishlist

--- src/pages/wishlist.tsx
+++ src/pages/wishlist.tsx
@@ -1,22 +1,18 @@
-export default function Wishlist() {
-  return <div>sad</div>
+import Wishlist, { WishlistTemplateProps } from 'templates/Wishlist'
+
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+export default function WishlistPage(props: WishlistTemplateProps) {
+  return <Wishlist {...props} />
 }
 
-// import Wishlist, { WishlistTemplateProps } from 'templates/Wishlist'
-
-// import gamesMock from 'components/GameCardSlider/mock'
-// import highlightMock from 'components/Highlight/mock'
-
-// export default function WishlistPage(props: WishlistTemplateProps) {
-//   return <Wishlist {...props} />
-// }
-
-// export async function getStaticProps() {
-//   return {
-//     props: {
-//       games: gamesMock,
-//       recommendedGames: gamesMock.slice(0, 5),
-//       recommendedHighlight: highlightMock
-//     }
-//   }
-// }
+export async function getStaticProps() {
+  return {
+    props: {
+      games: gamesMock,
+      recommendedGames: gamesMock.slice(0, 5),
+      recommendedHighlight: highlightMock
+    }
+  }
+}
