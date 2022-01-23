73ef76e8c006f0a3495febc97b3249549a493869
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-01-23 00:20:43 -0300

    feat: games page

--- src/pages/games.tsx
+++ src/pages/games.tsx
@@ -1,32 +1,72 @@
-import Games, { GamesTemplateProps } from 'templates/Games'
-import filterItemsMock from 'components/ExploreSidebar/mock'
 import { initializeApollo } from 'utils/apollo'
 import { QUERY_GAMES } from 'graphql/queries/games'
 import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
+import { parseQueryStringToWhere } from 'utils/filter'
+
+import GamesTemplate, { GamesTemplateProps } from 'templates/Games'
+import { GetServerSidePropsContext } from 'next'
+import {
+  genreFields,
+  platformFields,
+  priceFields,
+  sortFields
+} from 'utils/filter/fields'
 
 export default function GamesPage(props: GamesTemplateProps) {
-  return <Games {...props} />
+  return <GamesTemplate {...props} />
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps({ query }: GetServerSidePropsContext) {
   const apolloClient = initializeApollo()
 
-  const { data } = await apolloClient.query<QueryGames, QueryGamesVariables>({
+  const filterPrice = {
+    title: 'Price',
+    name: 'price_lte',
+    type: 'radio',
+    fields: priceFields
+  }
+
+  const filterPlatforms = {
+    title: 'Platforms',
+    name: 'platforms',
+    type: 'checkbox',
+    fields: platformFields
+  }
+
+  const filterSort = {
+    title: 'Sort by price',
+    name: 'sort',
+    type: 'radio',
+    fields: sortFields
+  }
+
+  const filterCategories = {
+    title: 'Genres',
+    name: 'categories',
+    type: 'checkbox',
+    fields: genreFields
+  }
+
+  const filterItems = [
+    filterSort,
+    filterPrice,
+    filterPlatforms,
+    filterCategories
+  ]
+
+  await apolloClient.query<QueryGames, QueryGamesVariables>({
     query: QUERY_GAMES,
-    variables: { limit: 9 }
+    variables: {
+      limit: 15,
+      where: parseQueryStringToWhere({ queryString: query, filterItems }),
+      sort: query.sort as string | null
+    }
   })
 
   return {
     props: {
-      revalidate: 60, //a cada 60s revalida a página
-      games: data.games.map((game) => ({
-        title: game.name,
-        slug: game.slug,
-        developer: game.developers[0].name,
-        img: `http://localhost:1337${game.cover!.url}`,
-        price: game.price
-      })),
-      filterItems: filterItemsMock
+      initialApolloState: apolloClient.cache.extract(),
+      filterItems
     }
   }
 }
