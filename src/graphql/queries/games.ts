3c6120efd94786cb7909b301f93bd2ec93d11794
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-01-23 00:20:27 -0300

    feat: games query

--- src/graphql/queries/games.ts
+++ src/graphql/queries/games.ts
@@ -1,24 +1,25 @@
-import { gql } from '@apollo/client'
+import { gql, QueryHookOptions, useQuery } from '@apollo/client'
+import { GameFragment } from 'graphql/fragments/game'
+import { QueryGames, QueryGamesVariables } from 'graphql/generated/QueryGames'
 
 export const QUERY_GAMES = gql`
-  query QueryGames($limit: Int!) {
-    games(limit: $limit) {
-      name
-      slug
-      cover {
-        url
+  query QueryGames($limit: Int, $start: Int, $where: JSON, $sort: String) {
+    games(limit: $limit, start: $start, where: $where, sort: $sort) {
+      ...GameFragment
+    }
+    gamesConnection(where: $where) {
+      values {
+        id
       }
-      developers {
-        name
-      }
-      price
     }
   }
+  ${GameFragment}
 `
 
 export const QUERY_GAME_BY_SLUG = gql`
   query QueryGameBySlug($slug: String!) {
     games(where: { slug: $slug }) {
+      id
       name
       short_description
       description
@@ -47,3 +48,9 @@ export const QUERY_GAME_BY_SLUG = gql`
     }
   }
 `
+
+export function useQueryGames(
+  options?: QueryHookOptions<QueryGames, QueryGamesVariables>
+) {
+  return useQuery<QueryGames, QueryGamesVariables>(QUERY_GAMES, options)
+}
