99001165379c5c507c4e0430236076d876a06b88
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-01-23 00:19:45 -0300

    feat: template/games

--- src/templates/Games/index.tsx
+++ src/templates/Games/index.tsx
@@ -1,45 +1,109 @@
-import Base from 'templates/Base'
+import { ParsedUrlQueryInput } from 'querystring'
+import { useRouter } from 'next/router'
 
+import { useQueryGames } from 'graphql/queries/games'
+import { parseQueryStringToFilter, parseQueryStringToWhere } from 'utils/filter'
+
+import Base from 'templates/Base'
 import { KeyboardArrowDown as ArrowDown } from '@styled-icons/material-outlined/KeyboardArrowDown'
 
 import ExploreSidebar, { ItemProps } from 'components/ExploreSidebar'
-import GameCard, { GameCardProps } from 'components/GameCard'
+import GameCard from 'components/GameCard'
 import { Grid } from 'components/Grid'
 
 import * as S from './styles'
+import Empty from 'components/Empty'
+import { getImageUrl } from 'utils/getImageUrl'
 
 export type GamesTemplateProps = {
-  games?: GameCardProps[]
   filterItems: ItemProps[]
 }
 
-const GamesTemplate = ({ filterItems, games = [] }: GamesTemplateProps) => {
-  const handleFilter = () => {
+const GamesTemplate = ({ filterItems }: GamesTemplateProps) => {
+  const { push, query } = useRouter()
+
+  const { data, loading, fetchMore } = useQueryGames({
+    notifyOnNetworkStatusChange: true,
+    variables: {
+      limit: 15,
+      where: parseQueryStringToWhere({ queryString: query, filterItems }),
+      sort: query.sort as string | null
+    }
+  })
+
+  if (!data) return <p>loading...</p>
+
+  const { games, gamesConnection } = data
+
+  const hasMoreGames = games.length < (gamesConnection?.values?.length || 0)
+
+  const handleFilter = (items: ParsedUrlQueryInput) => {
+    push({
+      pathname: '/games',
+      query: items
+    })
     return
   }
 
   const handleShowMore = () => {
-    return
+    fetchMore({ variables: { limit: 15, start: data?.games.length } })
   }
 
   return (
     <Base>
       <S.Main>
-        <ExploreSidebar items={filterItems} onFilter={handleFilter} />
-        <section>
-          <Grid>
-            {games.map((item) => (
-              <GameCard key={item.title} {...item} />
-            ))}
-          </Grid>
+        <ExploreSidebar
+          initialValues={parseQueryStringToFilter({
+            queryString: query,
+            filterItems
+          })}
+          items={filterItems}
+          onFilter={handleFilter}
+        />
 
-          <S.ShowMore role="button" onClick={handleShowMore}>
-            <p>Show More</p>
-            <ArrowDown size={35} />
-          </S.ShowMore>
+        <section>
+          {data?.games.length ? (
+            <>
+              <Grid>
+                {data?.games.map((game) => (
+                  <GameCard
+                    id={game.id}
+                    key={game.slug}
+                    title={game.name}
+                    slug={game.slug}
+                    developer={game.developers[0].name}
+                    img={`${getImageUrl(game?.cover?.url)}`}
+                    price={game.price}
+                  />
+                ))}
+              </Grid>
+              {hasMoreGames && (
+                <S.ShowMore>
+                  {loading ? (
+                    <S.ShowMoreLoading
+                      src="/img/dots.svg"
+                      alt="Loading more games..."
+                    />
+                  ) : (
+                    <S.ShowMoreButton onClick={handleShowMore}>
+                      <p>Show More</p>
+                      <ArrowDown size={35} />
+                    </S.ShowMoreButton>
+                  )}
+                </S.ShowMore>
+              )}
+            </>
+          ) : (
+            <Empty
+              title=":("
+              description="We didn't find any games with this filter"
+              hasLink
+            />
+          )}
         </section>
       </S.Main>
     </Base>
   )
 }
+
 export default GamesTemplate
--- src/templates/Games/styles.ts
+++ src/templates/Games/styles.ts
@@ -1,5 +1,7 @@
 import styled, { css } from 'styled-components'
 import media from 'styled-media-query'
+import { darken } from 'polished'
+
 import { Container } from 'components/Container'
 
 export const Main = styled(Container)`
@@ -13,16 +15,35 @@ export const Main = styled(Container)`
 `
 
 export const ShowMore = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 10rem;
+`
+
+export const ShowMoreButton = styled.button`
   ${({ theme }) => css`
-    color: ${theme.colors.white};
     text-align: center;
-    padding: ${theme.spacings.medium};
     text-transform: uppercase;
     font-weight: bold;
     cursor: pointer;
-
+    color: ${theme.colors.white};
+    background-color: transparent;
+    border: none;
+    padding: ${theme.spacings.xsmall};
+    transition: color ${theme.transition.default};
     > svg {
+      transition: color ${theme.transition.default};
       color: ${theme.colors.primary};
     }
+    &:hover {
+      color: ${darken(0.3, theme.colors.white)};
+      > svg {
+        color: ${darken(0.2, theme.colors.primary)};
+      }
+    }
   `}
+`
+export const ShowMoreLoading = styled.img`
+  width: 4rem;
 `
