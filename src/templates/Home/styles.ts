7699f141e5bc3dbc28259adb9b366b89bd55e990
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-02-20 21:52:09 -0300

    feat: remove unused style

--- src/templates/Home/styles.ts
+++ src/templates/Home/styles.ts
@@ -38,11 +38,3 @@ export const SectionNews = styled.div`
     `}
   `}
 `
-
-export const SectionUpcoming = styled.div`
-  ${({ theme }) => css`
-    ${HighlightStyles.Wrapper} {
-      margin-top: calc(${theme.spacings.xlarge} * 2);
-    }
-  `}
-`
