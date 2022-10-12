c279586b75013f06f810ede166fdfa99b9f4d8e3
Augusto S. Scher <augusto@jusbrasil.com.br>
2022-10-12 02:10:03 -0300

    refactor: add mappers for the page data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Home, { HomeTemplateProps } from 'templates/Home'
 import { initializeApollo } from 'utils/apollo'
 import { QueryHome } from 'graphql/generated/QueryHome'
 import { QUERY_HOME } from 'graphql/queries/home'
-import { getImageUrl } from 'utils/getImageUrl'
+import { bannerMapper, gamesMapper, highlightMapper } from 'utils/mappers'
 
 export default function Index(props: HomeTemplateProps) {
   return <Home {...props} />
@@ -23,77 +23,18 @@ export async function getStaticProps() {
   return {
     props: {
       revalidate: 10,
-      banners: banners.map((banner) => ({
-        img: getImageUrl(banner.image?.url),
-        title: banner.title,
-        subtitle: banner.subtitle,
-        buttonLabel: banner.button?.label,
-        buttonLink: banner.button?.link,
-        ...(banner.ribbon && {
-          ribbon: banner.ribbon.text,
-          ribbonColor: banner.ribbon.color,
-          ribbonSize: banner.ribbon.size
-        })
-      })),
+      banners: bannerMapper(banners),
       newGamesTitle: sections?.newGames?.title,
-      newGames: newGames.map((game) => ({
-        title: game.name,
-        slug: game.slug,
-        developer: game.developers[0].name,
-        img: getImageUrl(game.cover?.url),
-        price: game.price
-      })),
+      newGames: gamesMapper(newGames),
       mostPopularGamesTitle: sections?.popularGames?.title,
-      mostPopularHighlight: {
-        title: sections?.popularGames?.highlight?.title,
-        subtitle: sections?.popularGames?.highlight?.subtitle,
-        backgroundImage: getImageUrl(
-          sections?.popularGames?.highlight?.background?.url
-        ),
-        buttonLabel: sections?.popularGames?.highlight?.buttonLabel,
-        buttonLink: sections?.popularGames?.highlight?.buttonLink
-      },
-      mostPopularGames: sections!.popularGames!.games.map((game) => ({
-        title: game.name,
-        slug: game.slug,
-        developer: game.developers[0].name,
-        img: getImageUrl(game.cover?.url),
-        price: game.price
-      })),
+      mostPopularHighlight: highlightMapper(sections?.popularGames?.highlight),
+      mostPopularGames: gamesMapper(sections!.popularGames!.games),
       upcomingGamesTitle: sections?.upcomingGames?.title,
-      upcomingGames: upcomingGames.map((game) => ({
-        title: game.name,
-        slug: game.slug,
-        developer: game.developers[0].name,
-        img: getImageUrl(game.cover?.url),
-        price: game.price
-      })),
-      upcomingHighlight: {
-        title: sections?.upcomingGames?.highlight?.title,
-        subtitle: sections?.upcomingGames?.highlight?.subtitle,
-        backgroundImage: getImageUrl(
-          sections?.upcomingGames?.highlight?.background?.url
-        ),
-        buttonLabel: sections?.upcomingGames?.highlight?.buttonLabel,
-        buttonLink: sections?.upcomingGames?.highlight?.buttonLink
-      },
+      upcomingGames: gamesMapper(upcomingGames),
+      upcomingHighlight: highlightMapper(sections?.upcomingGames?.highlight),
       freeGamesTitle: sections?.freeGames?.title,
-      freeGames: freeGames.map((game) => ({
-        title: game.name,
-        slug: game.slug,
-        developer: game.developers[0].name,
-        img: getImageUrl(game.cover?.url),
-        price: game.price
-      })),
-      freeHighligth: {
-        title: sections?.freeGames?.highlight?.title,
-        subtitle: sections?.freeGames?.highlight?.subtitle,
-        backgroundImage: getImageUrl(
-          sections?.freeGames?.highlight?.background?.url
-        ),
-        buttonLabel: sections?.freeGames?.highlight?.buttonLabel,
-        buttonLink: sections?.freeGames?.highlight?.buttonLink
-      }
+      freeGames: gamesMapper(freeGames),
+      freeHighligth: highlightMapper(sections?.freeGames?.highlight)
     }
   }
 }
---
+++ src/utils/mappers.ts
@@ -0,0 +1,51 @@
+import {
+  QueryHome_banners,
+  QueryHome_newGames,
+  QueryHome_upcomingGames,
+  QueryHome_sections_popularGames_games,
+  QueryHome_freeGames,
+  QueryHome_sections_popularGames_highlight
+} from 'graphql/generated/QueryHome'
+import { getImageUrl } from 'utils/getImageUrl'
+
+export const bannerMapper = (banners: QueryHome_banners[]) =>
+  banners.map((banner) => ({
+    img: getImageUrl(banner.image?.url),
+    title: banner.title,
+    subtitle: banner.subtitle,
+    buttonLabel: banner.button?.label,
+    buttonLink: banner.button?.link,
+    ...(banner.ribbon && {
+      ribbon: banner.ribbon.text,
+      ribbonColor: banner.ribbon.color,
+      ribbonSize: banner.ribbon.size
+    })
+  }))
+
+export const gamesMapper = (
+  games:
+    | QueryHome_sections_popularGames_games[]
+    | QueryHome_upcomingGames[]
+    | QueryHome_freeGames[]
+    | QueryHome_newGames[]
+    | null
+    | undefined
+) =>
+  games &&
+  games.map((game) => ({
+    title: game.name,
+    slug: game.slug,
+    developer: game.developers[0].name,
+    img: getImageUrl(game.cover?.url),
+    price: game.price
+  }))
+
+export const highlightMapper = (
+  highlight: QueryHome_sections_popularGames_highlight | null | undefined
+) => ({
+  title: highlight?.title,
+  subtitle: highlight?.subtitle,
+  backgroundImage: getImageUrl(highlight?.background?.url),
+  buttonLabel: highlight?.buttonLabel,
+  buttonLink: highlight?.buttonLink
+})
